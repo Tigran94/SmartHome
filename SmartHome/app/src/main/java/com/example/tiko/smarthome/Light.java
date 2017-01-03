8c62d158b44a0dbaf9f528588e54b24d764cfca4
Tigran94 <tigran.torosyan@appricode.io>
2017-01-03 23:35:40 +0400

    first commit

---
+++ SmartHome/app/src/main/java/com/example/tiko/smarthome/Light.java
@@ -0,0 +1,97 @@
+package com.example.tiko.smarthome;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Light extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnKitchen,btnDining,btnBed,btnBath,btnKor;
+    boolean kitchen=true,dining=true,bed=true,bath=true,kor=true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_light);
+
+        btnKitchen=(Button)findViewById(R.id.btnKitchen);
+        btnDining=(Button)findViewById(R.id.btnDining);
+        btnBed=(Button)findViewById(R.id.btnBed);
+        btnBath=(Button)findViewById(R.id.btnBath);
+        btnKor=(Button)findViewById(R.id.btnKor);
+
+        btnKitchen.setOnClickListener(this);
+        btnDining.setOnClickListener(this);
+        btnBed.setOnClickListener(this);
+        btnBath.setOnClickListener(this);
+        btnKor.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btnKitchen:
+                if (kitchen) {
+                    btnKitchen.setBackgroundColor(Color.parseColor("#FBFF5E"));
+                    btnKitchen.setText("KITCHEN          TURN OFF");
+                    kitchen=false;
+                }
+                else {
+                    btnKitchen.setBackgroundColor(Color.TRANSPARENT);
+                    btnKitchen.setText("KITCHEN          TURN ON");
+                    kitchen=true;
+                }
+                break;
+            case R.id.btnDining:
+                if (dining) {
+                    btnDining.setBackgroundColor(Color.parseColor("#FBFF5E"));
+                    btnDining.setText("DINING ROOM   TURN OFF");
+                    dining=false;
+                }
+                else {
+                    btnDining.setBackgroundColor(Color.TRANSPARENT);
+                    btnDining.setText("DINING ROOM   TURN ON");
+                    dining=true;
+                }
+                break;
+            case R.id.btnBed:
+                if (bed) {
+                    btnBed.setBackgroundColor(Color.parseColor("#FBFF5E"));
+                    btnBed.setText("BED ROOM       TURN OFF");
+                    bed=false;
+                }
+                else {
+                    btnBed.setBackgroundColor(Color.TRANSPARENT);
+                    btnBed.setText("BED ROOM       TURN ON");
+                    bed=true;
+                }
+                break;
+            case R.id.btnBath:
+                if (bath) {
+                    btnBath.setBackgroundColor(Color.parseColor("#FBFF5E"));
+                    btnBath.setText("BATHROOM     TURN OFF");
+                    bath=false;
+                }
+                else {
+                    btnBath.setBackgroundColor(Color.TRANSPARENT);
+                    btnBath.setText("BATHROOM     TURN ON");
+                    bath=true;
+                }
+                break;
+            case R.id.btnKor:
+                if (kor) {
+                    btnKor.setBackgroundColor(Color.parseColor("#FBFF5E"));
+                    btnKor.setText("KORRIDOR     TURN OFF");
+                    kor=false;
+                }
+                else {
+                    btnKor.setBackgroundColor(Color.TRANSPARENT);
+                    btnKor.setText("KORRIDOR     TURN ON");
+                    kor=true;
+                }
+                break;
+        }
+    }
+}
---
+++ SmartHome/app/src/main/java/com/example/tiko/smarthome/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.tiko.smarthome;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLight,btnWindow,btnCurtain,btnSofa,btnConfig,btnExit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLight=(Button)findViewById(R.id.btnLight);
+        btnWindow=(Button)findViewById(R.id.btnWindow);
+        btnCurtain=(Button)findViewById(R.id.btnCurtain);
+        btnSofa=(Button)findViewById(R.id.btnSofa);
+        btnConfig=(Button)findViewById(R.id.btnConfig);
+        btnExit=(Button)findViewById(R.id.btnExit);
+
+        btnLight.setOnClickListener(this);
+        btnWindow.setOnClickListener(this);
+        btnCurtain.setOnClickListener(this);
+        btnSofa.setOnClickListener(this);
+        btnConfig.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.btnLight:
+                intent=new Intent(this,Light.class);
+                startActivity(intent);
+                break;
+            case R.id.btnWindow:
+                intent=new Intent(this,Window.class);
+                startActivity(intent);
+                break;
+            case R.id.btnCurtain:
+                intent=new Intent(this,Curtain.class);
+                startActivity(intent);
+                break;
+            case R.id.btnSofa:
+                intent=new Intent(this,Sofa.class);
+                startActivity(intent);
+                break;
+            case R.id.btnConfig:
+                intent=new Intent(this,Config.class);
+                startActivity(intent);
+                break;
+            case R.id.btnExit:
+                System.exit(1);
+        }
+    }
+}
