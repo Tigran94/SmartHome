8b58fabd8de33d7e4cb707c8f48a445bf5281b94
Tigran94 <tigran.torosyan@appricode.io>
2017-01-04 23:31:20 +0400

    5

--- SmartHome/app/src/main/java/com/example/tiko/smarthome/MainActivity.java
+++ SmartHome/app/src/main/java/com/example/tiko/smarthome/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnLight,btnWindow,btnCurtain,btnSofa,btnConfig,btnExit;
+    final int TRANSLATE_ID=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnConfig=(Button)findViewById(R.id.btnConfig);
         btnExit=(Button)findViewById(R.id.btnExit);
 
+
         btnLight.setOnClickListener(this);
         btnWindow.setOnClickListener(this);
         btnCurtain.setOnClickListener(this);
@@ -28,6 +32,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnConfig.setOnClickListener(this);
         btnExit.setOnClickListener(this);
 
+
+        Animation anim=null;
+        anim= AnimationUtils.loadAnimation(this,R.anim.alpha);
+        btnLight.startAnimation(anim);
+        btnWindow.startAnimation(anim);
+        btnConfig.startAnimation(anim);
+        btnCurtain.startAnimation(anim);
+        btnExit.startAnimation(anim);
+        btnSofa.startAnimation(anim);
     }
 
     @Override
