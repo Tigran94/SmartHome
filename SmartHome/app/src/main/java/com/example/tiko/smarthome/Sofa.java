be4f3d6669804da5e10e57b4bcb44283371f6c68
Tigran94 <tigran.torosyan@appricode.io>
2017-01-04 19:53:04 +0400

    curtain,sofa

--- SmartHome/app/src/main/java/com/example/tiko/smarthome/Sofa.java
+++ SmartHome/app/src/main/java/com/example/tiko/smarthome/Sofa.java
@@ -2,12 +2,39 @@ package com.example.tiko.smarthome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class Sofa extends AppCompatActivity {
 
+    SeekBar seekBar;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sofa);
+
+
+        seekBar = (SeekBar) findViewById(R.id.seekBarSofa);
+        textView = (TextView) findViewById(R.id.countSofa);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int seekBarProgress = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBarProgress = progress;
+
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textView.setText("Progress: " + seekBarProgress + " / " + seekBar.getMax());
+                // Toast.makeText(getApplicationContext(), "SeekBar Touch Stop ", Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
